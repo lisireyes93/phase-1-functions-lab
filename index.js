39abcada9a92567f4ceed5cf9842c9b362bc0921
Alicia Moreno <lisireyes93@yahoo.com>
2021-07-20 22:44:17 -0500

    commit tree d0a91457f822ed9d2ee82bbe7bc0a150f60ce6e9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation){
+    if (pickUpLocation > 42){
+        return pickUpLocation - 42
+    }
+    else {
+        return 42 - pickUpLocation
+    }
+}
+
+function distanceFromHqInFeet(pickUpLocation){
+    return distanceFromHqInBlocks(pickUpLocation) * 264
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    if(startBlock > endBlock){
+        return 264 * (startBlock - endBlock)
+    }
+    else {
+        return 264 * (endBlock - startBlock)
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start, destination) < 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500){
+        return 25
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
